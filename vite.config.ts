c61fee324f4da957bd9791ad3079f18a9b36d619
ykwen <ykwen@iflytek.com>
2023-12-15 23:42:49 +0800

    chore: auto release

--- vite.config.ts
+++ vite.config.ts
@@ -29,9 +29,13 @@ export default defineConfig({
         presetUno(),
         presetAttributify()
       ],
-      include: ["./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-        "./node_modules/primevue/**/*.{vue,js,ts,jsx,tsx}"]
+      content: {
+        pipeline: {
+          include: ["./index.html",
+            "./src/**/*.{vue,js,ts,jsx,tsx}",
+            "./node_modules/primevue/**/*.{vue,js,ts,jsx,tsx}"]
+        }
+      }
     })
   ],
 })
